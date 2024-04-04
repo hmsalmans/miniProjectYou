a327f680bff3414a173da6b595d73f1882d770ca
Salman <hmsalmans1@gmail.com>
2024-04-04 10:41:21 +0500

    finalized the ini project

--- src/apuCodes/ApuCodesTesting.java
+++ src/apuCodes/ApuCodesTesting.java
@@ -1,5 +1,7 @@
 package apuCodes;
 
+import java.lang.Math;
+import java.util.Scanner;
 public class ApuCodesTesting {  // THIS IS CLASS NAME
 	
 	
@@ -17,7 +19,7 @@ public class ApuCodesTesting {  // THIS IS CLASS NAME
 		
 		System.out.println(fnalName.length()); // length method
 		
-		
+		//---------------------
 		int a = 23;
 		int b = 7;
 		
@@ -25,7 +27,57 @@ public class ApuCodesTesting {  // THIS IS CLASS NAME
 		
 		int modu = a%b; // answer should be 2
 		System.out.println(modu);
+		
+		//-----------------
+		
+		int c = 10;
+		int d = 20;
+	int res =	Math.max(c, d);
+		System.out.println(res);
+		
+		System.out.println(Math.min(c, d));
+		//------------------------- Taking Input using scanner class
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("What is your age---Please Type");
+		
+		int	inptReplied = sc.nextInt();
+		
+		System.out.println("So you age is "+ inptReplied);
+		//--------------------------------
+		
+		int i;
+		//lets practice continue keyword
+		 for (i = 2; i <=20; i=i+2) {
+			 if (i%3==0)  { // taking modulo here
+				 continue;//skipping this and moving next iteration
+			 }
+			 
+			 System.out.println(i); // answer should be 2,4,8,10,14,16,20
+			 
+		 }
+		//lets practice break keyword
+		 
+		 for (i=1; i<=10; i++) {
+			 
+			 	if(i<5) {
+				 System.out.println(i);
+			 }	
+			 	else if (i>=5) {
+			 	 System.out.println(i+10);	
+			 }
+			 	if (i==8) {
+			 		System.out.println("It should break after i reaches 8");
+			 		break;
+		    }
+		 }	 	
+		//--------------------------------
+		 
+		 
+		
+		
 
-	}
-
+	 }
+	
 }
